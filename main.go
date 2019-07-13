039683d0f948a844538bdc5c1094a35fe5aecb09
Max Kulish <kma.memo@gmail.com>
2019-07-13 10:21:59 +0300

    Handler added

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("Hello World")
+func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main()  {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
