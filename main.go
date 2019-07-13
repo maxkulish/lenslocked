58845f23b18bbf4979bb10f4e385ddfc65643269
Max Kulish <kma.memo@gmail.com>
2019-07-13 11:22:48 +0300

    Added a contact page

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		_, _ = fmt.Fprint(w, "To get in touch, please send an email to <"+
+			"a href=\"mailto:m@example.com\">m@example.com")
+	}
+
 }
 
 func main() {
