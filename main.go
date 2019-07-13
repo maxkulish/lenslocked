50e726c7e6386a88f3096048640c3d02a1fb5048
Max Kulish <kma.memo@gmail.com>
2019-07-13 10:46:13 +0300

    Ignoring results

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func main()  {
+func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	_ = http.ListenAndServe(":3000", nil)
 }
