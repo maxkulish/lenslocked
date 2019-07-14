0dda20f04f2b1c0fc034d4e164b071e7e033b6ea
Max Kulish <kma.memo@gmail.com>
2019-07-14 21:07:11 +0300

    Gorilla mux routing

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module lenslocked
 
 go 1.12
 
-require github.com/julienschmidt/httprouter v1.2.0
+require github.com/gorilla/mux v1.7.3
--- main.go
+++ main.go
@@ -4,34 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		_, _ = fmt.Fprint(w, "To get in touch, please send an email to <"+
-			"a href=\"mailto:m@example.com\">m@example.com")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		_, _ = fmt.Fprint(w, "<h1>We could not find the page you were looking for :( ")
-	}
-
+	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
-}
-
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	_, _ = fmt.Fprint(w, "To get in touch, please send an email to <"+
+		"a href=\"mailto:m@example.com\">m@example.com")
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", Index)
-	router.GET("/hello/:name/spanish", Hello)
-	_ = http.ListenAndServe(":3000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	_ = http.ListenAndServe(":3000", r)
 }
