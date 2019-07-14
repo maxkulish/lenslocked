447ddf87f1fb733589330d31ea9fafd9d37b86f8
Max Kulish <kma.memo@gmail.com>
2019-07-14 21:37:39 +0300

    mux replaced by httprouter

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module lenslocked
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/julienschmidt/httprouter v1.2.0
+)
--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	_, _ = fmt.Fprint(w, "To get in touch, please send an email to <"+
 		"a href=\"mailto:m@example.com\">m@example.com")
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	_, _ = fmt.Fprint(w, "<h1>This is my FAQ page</h1>")
 
@@ -30,13 +30,13 @@ func notFound404(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
 
-	// Custom 404 response
-	handler := http.HandlerFunc(notFound404)
-	r.NotFoundHandler = handler
+	handler404 := http.HandlerFunc(notFound404)
+	r.NotFound = handler404
+
 	_ = http.ListenAndServe(":3000", r)
 }
