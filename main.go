299bf5ad95c1fae1f665230885fbff4423da041c
Max Kulish <kma.memo@gmail.com>
2019-07-14 21:24:59 +0300

    Custom 404 page in Gorilla/mux router

--- main.go
+++ main.go
@@ -18,9 +18,25 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"a href=\"mailto:m@example.com\">m@example.com")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	_, _ = fmt.Fprint(w, "<h1>This is my FAQ page</h1>")
+
+}
+
+func notFound404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	_, _ = fmt.Fprint(w, "I can't find this page")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+
+	// Custom 404 response
+	handler := http.HandlerFunc(notFound404)
+	r.NotFoundHandler = handler
 	_ = http.ListenAndServe(":3000", r)
 }
