b140a4c84a02c4926560eddf3cb869a253cbf7ca
Max Kul <kma.memo@gmail.com>
2019-08-27 12:42:03 +0300

    Implemented the Title and UserID required validators

--- models/errors.go
+++ models/errors.go
@@ -5,24 +5,25 @@ import "strings"
 var (
 	// ErrEmailRequited is returned when an email address is not provided
 	ErrEmailRequited modelError = "email address is required"
-
 	// ErrEmailInvalid is returned when an email address provided
 	// does not match any of our requirements
 	ErrEmailInvalid modelError = "email address is not valid"
-
 	// ErrPasswordTooShort is returned when an update or create is
 	// attempted with a user password that is less than 8 characters
 	ErrPasswordTooShort modelError = "password must be at least 8 characters"
-
 	// ErrPasswordRequired is returned when an user password field is not provided
 	ErrPasswordRequired modelError = "password is required"
+	// ErrTitleRequired
+	ErrTitleRequired modelError = "models: title is required"
 
+	// Private Errors
 	// ErrRememberTooShort is returned when a remember token is
 	// not at least 32 bytes
-	ErrRememberTooShort modelError = "remember token must be at least 32 bytes"
-
+	ErrRememberTooShort privateError = "remember token must be at least 32 bytes"
 	// ErrRememberRequired
-	ErrRememberRequired modelError = "remember hash is required"
+	ErrRememberRequired privateError = "remember hash is required"
+	// ErrUserIDRequired
+	ErrUserIDRequired privateError = "models: user ID is required"
 )
 
 type modelError string
@@ -36,4 +37,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
--- models/galleries.go
+++ models/galleries.go
@@ -32,10 +32,48 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFuncs(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+
+	return nil
+}
+
 type galleryGorm struct {
 	db *gorm.DB
 }
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFunc func(*Gallery) error
+
+func runGalleryValFuncs(gallery *Gallery, fns ...galleryValFunc) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
 }
